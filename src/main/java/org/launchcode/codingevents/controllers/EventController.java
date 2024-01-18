d598afcfb0c67d1c2d003ed85e2fef069cc1560d
Kevin Wiegman <wiegmank@gmail.com>
2024-01-17 22:11:53 -0600

    workalong complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -3,23 +3,44 @@ package org.launchcode.codingevents.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
+    private static List<String> events = new ArrayList<>();
     @GetMapping
-    public String events(Model model) {
-        ArrayList<String> eventNames = new ArrayList<>();
-        eventNames.add("Stanley Cup");
-        eventNames.add("World Cup");
-        eventNames.add("Superbowl");
-        eventNames.add("World Series");
-
-        model.addAttribute("events", eventNames);
+    public String displayAllEvents(Model model) {
+//        List<String> eventNames = new ArrayList<>();
+//        eventNames.add("Stanley Cup");
+//        eventNames.add("World Cup");
+//        eventNames.add("Superbowl");
+//        eventNames.add("World Series");
+//
+//        model.addAttribute("events", eventNames);
+        model.addAttribute("events", events);
         return "events/index";
     }
+
+    // located @ /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    //lives at /events/create
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
+
+    }
+
+
 }
