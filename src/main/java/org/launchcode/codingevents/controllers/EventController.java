99b31399141b47e968977763784d4f0082f99640
Kevin Wiegman <wiegmank@gmail.com>
2024-01-24 20:30:46 -0600

    exercise complete

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,8 +1,10 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 
@@ -26,11 +28,17 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute("event", new Event());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            model.addAttribute("errorMsg", "Bad data!");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
     }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,5 +1,9 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.util.Objects;
 
 /**
@@ -10,15 +14,30 @@ public class Event {
     private int id;
     private static int nextId = 1;
 
+
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank
     private String name;
+
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+
+    public Event(String name, String description, String contactEmail) {
+        this();
         this.name = name;
         this.description = description;
+        this.contactEmail = contactEmail;
+
+    }
+
+    public Event(){
         this.id = nextId;
         nextId++;
-    }
+    };
 
     public String getName() {
         return name;
@@ -35,6 +54,10 @@ public class Event {
     public void setDescription(String description) {
         this.description = description;
     }
+
+    public String getContactEmail() { return contactEmail; }
+
+    public void setContactEmail(String contactEmail) { this.contactEmail = contactEmail; }
 
     public int getId() {
         return id;
