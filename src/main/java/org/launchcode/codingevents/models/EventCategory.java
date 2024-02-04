b3f051cb5fe3b6d5961068570379b54ab53e5d0b
Kevin Wiegman <wiegmank@gmail.com>
2024-02-04 14:03:23 -0600

    exercises complete

---
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -0,0 +1,53 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
+@Entity
+public class EventCategory {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Size(min=3, message="Name must be at least 3 characters")
+    private String name;
+
+    public EventCategory(@Size(min=3, message="Name must be at least 3 characters") String name) {
+        this.name = name;
+    }
+
+    public EventCategory() {}
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EventCategory that = (EventCategory) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
