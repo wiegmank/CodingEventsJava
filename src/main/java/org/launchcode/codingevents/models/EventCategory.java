eedaac048434edcc7303a7d06d5ea3e45e3278cb
Kevin Wiegman <wiegmank@gmail.com>
2024-02-08 07:32:27 -0600

    orm codealong

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,6 +1,7 @@
 package org.launchcode.codingevents.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.Size;
 import java.util.ArrayList;
@@ -15,6 +16,9 @@ public class EventCategory extends AbstractEntity {
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
+    @OneToMany(mappedBy = "eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
     }
@@ -27,6 +31,10 @@ public class EventCategory extends AbstractEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     @Override
